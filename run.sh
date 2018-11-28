30d9e2bddb873f6c6bd7bec386fe9b17b861f974
SP Mohanty <spmohanty91@gmail.com>
2018-11-28 17:34:03 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Use This line would work in case of the docker container
-# Else the relevant env variables will not be loaded during evaluatino
-source activate root 
-echo "root"
+if [[ -z "${CROWDAI_MARLO_GRADING}" ]]; then
+	source activate marlo #Use This line would work in case of local debugs;
+	# in a local debug setup, you should use your env name instead of marlo
+	echo "marlo"
+else
+	source activate root #Use This line would work in case of the docker container
+	echo "root"
+fi
 
 python run.py
